11acce4c8fa8272e1e8c578cc5c828353ba61482
Andrea <andreacornaglia@gmail.com>
2015-04-18 16:41:07 -0700

    tabs working but not looking good

--- app/src/main/java/andreacornaglia/net/foodpyramid/FruitFragment.java
+++ app/src/main/java/andreacornaglia/net/foodpyramid/FruitFragment.java
@@ -13,7 +13,7 @@ public class FruitFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vegetable, container, false);
+        return inflater.inflate(R.layout.fragment_fruit, container, false);
     }
 
 }
