ee49b2a0e2c1574a134513d97a0baf43cc4720b5
durga <durga.gm@gmail.com>
2015-04-29 22:07:55 -0700

    Added calculate pyramid functionality

--- app/src/main/java/andreacornaglia/net/foodpyramid/AddFoodActivity.java
+++ app/src/main/java/andreacornaglia/net/foodpyramid/AddFoodActivity.java
@@ -1,11 +1,13 @@
 package andreacornaglia.net.foodpyramid;
 
+import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class AddFoodActivity extends ActionBarActivity {
@@ -45,5 +47,11 @@ public class AddFoodActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void CalculatePyramid(View view)
+    {
+        Intent in = new Intent(this, MyPyramidActivity.class);
+        startActivity(in);
     }
 }
